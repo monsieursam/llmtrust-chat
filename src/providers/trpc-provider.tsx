46262b5645aae529a2aa24e76be745b482eb60a7
Sam <samuel.s.mamane@gmail.com>
2025-04-03 17:05:44 +0200

    Retryyyy

--- src/providers/trpc-provider.tsx
+++ src/providers/trpc-provider.tsx
@@ -13,9 +13,8 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
+        staleTime: 60 * 1000, // 1 minute
+        refetchOnWindowFocus: false,
       },
     },
   });
